068030b261e21d6e7d59ac9604518f1b16bc8540
JFogotcheck <fogotcheck@live.com>
2024-10-30 17:57:47 +0300

    Task_1: add exe

--- Vasiliev-6_edition/Chapter_1/Task_1/AppMain/Main.cpp
+++ Vasiliev-6_edition/Chapter_1/Task_1/AppMain/Main.cpp
@@ -4,6 +4,25 @@ using namespace std;
 
 int main(void)
 {
-	cout << "Hello" << endl;
+	cout << "a = arcsin((g * T)/(2 * V))" << endl;
+	cout << "Input::\tV,T" << endl;
+	double V = 0, T = 0;
+	const double g = 9.8;
+	cin >> V >> T;
+	if (V == 0) {
+		cout << "Div zero" << endl;
+		return -1;
+	}
+
+	double tmp = (g * T) / (2 * V);
+	if (abs(tmp) > 1) {
+		cout << "|(" << g << " * " << T << ") / (2 * " << V
+		     << ")| = " << tmp << " > 1" << endl;
+		return -1;
+	}
+
+	cout << "a = arcsin((" << g << " * " << T << ")/(2 * " << V
+	     << ")) = " << asin(tmp) << " rad\t" << (asin(tmp) * (180 / M_PI))
+	     << " deg" << endl;
 	return 0;
 }
--- Vasiliev-6_edition/Chapter_1/Task_1/AppMain/Main.h
+++ Vasiliev-6_edition/Chapter_1/Task_1/AppMain/Main.h
@@ -1,6 +1,8 @@
 #ifndef __Main_h__
 #define __Main_h__
 
+#define _USE_MATH_DEFINES
 #include <iostream>
+#include <cmath>
 
 #endif //__Main_h__
