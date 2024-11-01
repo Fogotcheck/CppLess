8610978a46b3444dc6860d692809a27cb308498f
JFogotcheck <fogotcheck@live.com>
2024-11-01 13:58:02 +0300

    Task_2: add exe

--- Vasiliev-6_edition/Chapter_1/Task_2/AppMain/Main.cpp
+++ Vasiliev-6_edition/Chapter_1/Task_2/AppMain/Main.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main(void)
 {
-	cout << "Hello" << endl;
+	cout << "Distance calculation for projectile motion\n";
+	cout << "Formula: L = (V^2 * sin(2a)) / g\n";
+	cout << "Please enter initial speed (V) in m/s and angle (a) in degrees:\n";
+
+	double V = 0, Alpha = 0;
+	const double g = 9.8;
+
+	cin >> V >> Alpha;
+
+	cout << fixed << setprecision(4);
+
+	cout << "L = (" << V << "^2 * sin(2* " << Alpha << ")) / " << g << " = "
+	     << (V * V * sin(2 * (Alpha * M_PI / 180))) / g << endl;
+
 	return 0;
 }
